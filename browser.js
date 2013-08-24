f47f4cf4cdd670b64f6fd0b7bda52d8b9e325995
Ignacio Peinado <i.peinado.martinez@gmail.com>
2013-08-24 18:34:56 +0200

    added url bar with simple url validation to the browser

--- browser.js
+++ browser.js
@@ -1,7 +1,23 @@
 onload = function() {
-	var webview.addEventListener("loadstop", loadstop);
+  
+	var webview = document.querySelector('#browser_content');
+	var urlinput = document.querySelector('#url');
+	
+	document.querySelector('#url-form').onsubmit = function(e) {
+	  e.preventDefault();
+	  var url = validateUrl(urlinput.value);
+	  urlinput.value = url;
+	  webview.src = url;
+	}
 }
 
-var loadstop = function() {
-	
+var validateUrl = function(url) {
+  var urlregex = new RegExp("http:\/\/");
+      if (urlregex.test(url)) {
+          return url;
+      }
+      return "http://" + url;
 }
+
+	
+
